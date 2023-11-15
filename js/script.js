6fae602c773e69927f6d1ce843d0e093d2f94036
Samruddhi Kakade <skakade2@hawk.iit.edu>
2023-11-14 23:29:15 -0500

    Added javascript

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function initMap() {
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 41.8349, lng: -87.6270 }, 
+      zoom: 15, 
+    });
+  
+    const marker = new google.maps.Marker({
+      position: { lat: 41.8349, lng: -87.6270 }, 
+      map: map,
+      title: 'Illinois Institute of Technology',
+    });
+  
+    const infoWindow = new google.maps.InfoWindow({
+      content: 'Illinois Institute of Technology',
+    });
+  
+    google.maps.event.addListener(marker, 'click', function () {
+      infoWindow.open(map, marker);
+    });
+  }
+  
