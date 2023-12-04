0d540a001c707d6060f5dc02a34b1976580dfdd6
Samruddhi Kakade <skakade2@hawk.iit.edu>
2023-12-03 23:30:31 -0500

    Added slider script

--- js/script.js
+++ js/script.js
@@ -17,5 +17,18 @@ function initMap() {
     google.maps.event.addListener(marker, 'click', function () {
       infoWindow.open(map, marker);
     });
+
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
   }
   
